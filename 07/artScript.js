322ce519976aa31df8f398d5ad637c89fb27f3ad
Manuel Luppino <luppinomanuel@gmail.com>
2023-03-07 14:58:54 +0100

    exercise

---
+++ 07/artScript.js
@@ -0,0 +1,16 @@
+const figlet = require('figlet');
+
+figlet.text('Ciao Patrizio', {
+    font: 'The Edge',
+    horizontalLayout: 'default',
+    verticalLayout: 'default',
+    width: 100,
+    whitespaceBreak: true
+}, function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data);
+});
